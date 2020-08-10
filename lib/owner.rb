8f0140a66a5a41e400cbbb717e799d2b951a5f9a
Eugene O'Neil <ejo7220@gmail.com>
2020-08-10 22:49:14 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all 
+    @@all.clear
+  end
+  
+  def say_species
+   "I am a human."
+  end
 end
